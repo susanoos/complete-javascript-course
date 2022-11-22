7ab504ca5e7e0c46a1288a4a0a77af178728cffa
David <escxbar@gmail.com>
2022-11-21 19:59:22 -0800

    finished the project

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -7,6 +7,7 @@ const p2CurrentScore = document.getElementById('current--1');
 const player2Score = document.getElementById('score--1');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHoldDice = document.querySelector('.btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
 const dice = document.querySelector('.dice');
 let player1CurrentScore = 0;
 let player2CurrentScore = 0;
@@ -23,32 +24,99 @@ const rollDice = () => {
   return dice;
 };
 
-btnRollDice.addEventListener('click', () => {
-  let randomDice = rollDice();
-  dice.src = `dice-${randomDice}.png`;
-  if (player1.classList.contains('player--active')) {
-    player1CurrentScore += randomDice;
-    p1CurrentScore.textContent = player1CurrentScore;
-  } else {
-    player2CurrentScore += randomDice;
-    p2CurrentScore.textContent = player2CurrentScore;
-  }
-});
+// Game logic function
+const game = () => {
+  btnRollDice.addEventListener('click', () => {
+    let randomDice = rollDice();
+    // sets randome dice image
+    dice.src = `dice-${randomDice}.png`;
+    // checks if it is player 1's turn by checking the classList and if the dice is not equal to 1 then player 1 keeps playing
+    if (player1.classList.contains('player--active') && randomDice != 1) {
+      player1CurrentScore += randomDice;
+      p1CurrentScore.textContent = player1CurrentScore;
 
-btnHoldDice.addEventListener('click', () => {
-  if (player1.classList.contains('player--active')) {
-    p1Score += player1CurrentScore;
-    player1Score.textContent = p1Score;
-    player1CurrentScore = 0;
-    p1CurrentScore.textContent = player1CurrentScore;
-    player1.classList.remove('player--active');
-    player2.classList.add('player--active');
-  } else {
-    p2Score += player2CurrentScore;
-    player2Score.textContent = p2Score;
-    player2CurrentScore = 0;
-    p2CurrentScore.textContent = player2CurrentScore;
-    player2.classList.remove('player--active');
+      // same logic as player 1 but for player 2
+    } else if (
+      player2.classList.contains('player--active') &&
+      randomDice != 1
+    ) {
+      player2CurrentScore += randomDice;
+      p2CurrentScore.textContent = player2CurrentScore;
+
+      // checks if the dice is equal to one and if it is then it will switch player
+    } else if (
+      player1.classList.contains('player--active') &&
+      randomDice === 1
+    ) {
+      player1CurrentScore = 0;
+      p1CurrentScore.textContent = player1CurrentScore;
+      player1.classList.remove('player--active');
+      player2.classList.add('player--active');
+
+      // same as the logic above
+    } else if (
+      player2.classList.contains('player--active') &&
+      randomDice === 1
+    ) {
+      player2CurrentScore = 0;
+      p2CurrentScore.textContent = player2CurrentScore;
+      player2.classList.remove('player--active');
+      player1.classList.add('player--active');
+    }
+  });
+};
+
+// hold button function
+const hold = () => {
+  btnHoldDice.addEventListener('click', () => {
+    // will save the current score for player 1 and switch to player 2 and will remove 'player--active' class and add to player 2
+    if (player1.classList.contains('player--active')) {
+      p1Score += player1CurrentScore;
+      player1Score.textContent = p1Score;
+      player1CurrentScore = 0;
+      p1CurrentScore.textContent = player1CurrentScore;
+      player1.classList.remove('player--active');
+      player2.classList.add('player--active');
+
+      // same as the logic above but for player 2
+    } else {
+      p2Score += player2CurrentScore;
+      player2Score.textContent = p2Score;
+      player2CurrentScore = 0;
+      p2CurrentScore.textContent = player2CurrentScore;
+      player2.classList.remove('player--active');
+      player1.classList.add('player--active');
+    }
+
+    // winning conditions will add to the player with the score equal to or greater than, adds 'player--winner' class and removes 'player--active' class
+    if (p1Score >= 10) {
+      player1.classList.add('player--winner');
+      player2.classList.remove('player--active');
+    } else if (p2Score >= 10) {
+      player2.classList.add('player--winner');
+      player1.classList.remove('player--active');
+    }
+  });
+};
+
+// new game function resets all values and classes to default, player 1 is the active player
+const newGame = () => {
+  btnNewGame.addEventListener('click', () => {
     player1.classList.add('player--active');
-  }
-});
+    player2.classList.remove('player--active');
+    player1.classList.remove('player--winner');
+    player2.classList.remove('player--winner');
+    player1CurrentScore = 0;
+    player2CurrentScore = 0;
+    p1Score = 0;
+    p2Score = 0;
+    p1CurrentScore.textContent = player1CurrentScore;
+    p2CurrentScore.textContent = player2CurrentScore;
+    player1Score.textContent = player1CurrentScore;
+    player2Score.textContent = player2CurrentScore;
+  });
+};
+
+game();
+hold();
+newGame();
